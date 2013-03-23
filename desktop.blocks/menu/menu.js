3deddf57028c968794a6f57491fe73e6994ea08e
Denis Hananein <i@zloylos.me>
2013-03-24 01:25:54 +0400

    Доделал логику

--- desktop.blocks/menu/menu.js
+++ desktop.blocks/menu/menu.js
@@ -1,5 +1,10 @@
 BEM.DOM.decl('menu', {
     onElemSetMod: {
+        'js': function () {
+            var groups = this.findBlockOutside('menu');
+            BEM.blocks['menu']
+                .liveCtxBind(groups, 'menuItemClick', function (e, data) { console.log(data) } );
+        },
         //Будем реагировать на изменение состояния элемента item
         'item': {
             // когда у него будет меняться модификатор state,
@@ -47,11 +52,19 @@ BEM.DOM.decl('menu', {
 
         // Сворачиваем группу.
         groupEl.slideToggle();
-        // Триггаем о сворачивании.
+        //  Говорим, что сворачиваем. 
         this.toggleMod(groupEl, 'state', 'fold')
         // Выделяем заголовок группы
-        this.toggleMod(el, 'state', 'active');
+        this.toggleMod(el, 'state', 'fold');
+    },
+
+    _lastItemToggleMod: function (elem) {
+        console.log(elem)
+        this.__base.apply(this, arguments);
+        this.lastSelected = elem;
+        this.delMod(this.lastSelected, 'state');
     }
+
 }, {
     live: function () {
         // Вешаем слушатель на клик.
@@ -61,6 +74,13 @@ BEM.DOM.decl('menu', {
 
         this.liveBindTo('title', 'click', function (e) {
             this.onTriggerGroupClick(e);
+        });
+
+        this.on('menuItemClick', function (e, data) {
+            // Педалька. 
+            // Почему-то не срабатывает delMod.
+            this.lastSelected && this.lastSelected.removeClass('menu__item_state_active');
+            this.lastSelected = data.domElem;
         });
     }
 });
