9ec3b90d3e9561ef51c75b74ba1cb1181c04817b
Denis Hananein <i@zloylos.me>
2013-03-25 15:29:56 +0400

    Сборка примера

---
+++ desktop.bundles/index/index.bemdecl.js
@@ -0,0 +1,47 @@
+exports.blocks = [
+    {
+        "name": "b-page"
+    },
+    {
+        "name": "container"
+    },
+    {
+        "name": "i-geo",
+        "elems": [
+            {
+                "name": "map"
+            },
+            {
+                "name": "menu"
+            }
+        ]
+    },
+    {
+        "name": "map",
+        "mods": [
+            {
+                "name": "api",
+                "vals": [
+                    "ymaps"
+                ]
+            }
+        ]
+    },
+    {
+        "name": "sidebar"
+    },
+    {
+        "name": "menu",
+        "elems": [
+            {
+                "name": "title"
+            },
+            {
+                "name": "content"
+            },
+            {
+                "name": "item"
+            }
+        ]
+    }
+];
---
+++ desktop.bundles/index/index.deps.js
@@ -0,0 +1,153 @@
+exports.deps = [
+    {
+        "block": "i-jquery"
+    },
+    {
+        "block": "i-jquery",
+        "elem": "inherit"
+    },
+    {
+        "block": "i-jquery",
+        "elem": "identify"
+    },
+    {
+        "block": "i-jquery",
+        "elem": "is-empty-object"
+    },
+    {
+        "block": "i-jquery",
+        "elem": "debounce"
+    },
+    {
+        "block": "i-jquery",
+        "elem": "observable"
+    },
+    {
+        "block": "i-bem"
+    },
+    {
+        "block": "i-ecma",
+        "elem": "object"
+    },
+    {
+        "block": "i-ecma",
+        "elem": "array"
+    },
+    {
+        "block": "i-ecma",
+        "elem": "function"
+    },
+    {
+        "block": "i-bem",
+        "elem": "internal"
+    },
+    {
+        "block": "bemhtml"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom"
+    },
+    {
+        "block": "i-ecma",
+        "elem": "string"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom",
+        "mod": "init"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom",
+        "mod": "init",
+        "val": "auto"
+    },
+    {
+        "block": "i-ua"
+    },
+    {
+        "block": "i-jquery",
+        "elem": "core"
+    },
+    {
+        "block": "b-page"
+    },
+    {
+        "block": "b-page",
+        "elem": "css"
+    },
+    {
+        "block": "b-page",
+        "elem": "js"
+    },
+    {
+        "block": "container"
+    },
+    {
+        "block": "menu"
+    },
+    {
+        "block": "map"
+    },
+    {
+        "block": "i-geo"
+    },
+    {
+        "block": "i-geo",
+        "elem": "map"
+    },
+    {
+        "block": "i-geo",
+        "elem": "menu"
+    },
+    {
+        "block": "map",
+        "mod": "api"
+    },
+    {
+        "block": "map",
+        "mod": "api",
+        "val": "ymaps"
+    },
+    {
+        "block": "sidebar"
+    },
+    {
+        "block": "menu",
+        "elem": "title"
+    },
+    {
+        "block": "menu",
+        "elem": "title",
+        "mod": "state"
+    },
+    {
+        "block": "menu",
+        "elem": "title",
+        "mod": "state",
+        "val": "fold"
+    },
+    {
+        "block": "menu",
+        "elem": "content"
+    },
+    {
+        "block": "menu",
+        "elem": "item"
+    },
+    {
+        "block": "menu",
+        "elem": "item",
+        "mod": "state"
+    },
+    {
+        "block": "menu",
+        "elem": "item",
+        "mod": "state",
+        "val": "active"
+    }
+];
+exports.depsByTechs = {
+    "": {}
+};
